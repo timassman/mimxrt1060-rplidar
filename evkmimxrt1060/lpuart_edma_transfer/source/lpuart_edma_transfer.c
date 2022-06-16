ca7484d2727d5bd5b09a5ac14314937b225426ea
timassman <t.m.assman@gmail.com>
2022-06-16 07:14:30 +0200

    Add rplidar_sdk as a git module, and try to include SDK header file
    However, I need a wrapper to include the C++ rplidar_sdk in my C project

--- evkmimxrt1060/lpuart_edma_transfer/source/lpuart_edma_transfer.c
+++ evkmimxrt1060/lpuart_edma_transfer/source/lpuart_edma_transfer.c
@@ -14,6 +14,17 @@
 #include "fsl_dmamux.h"
 #endif
 #include "sw_delay.h"
+
+// rplidar_sdk
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+
+#include "sl_lidar.h"
+#include "sl_lidar_driver.h"
+
+
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
